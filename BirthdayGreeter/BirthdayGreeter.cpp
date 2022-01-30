a06017c9f32784e539f83fd0512f22fcc09677dd
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-01-29 20:21:32 -0800

    Add files via upload

--- BirthdayGreeter/BirthdayGreeter.cpp
+++ BirthdayGreeter/BirthdayGreeter.cpp
@@ -11,13 +11,26 @@ using namespace std;
 // parent class
 class TheDate {
 public:
-	int	month, day, year;
+
+	// define the struc to
+	// hold the dates
+	struct Date {
+		int	
+			month, 
+			day, 
+			year;
+	};
+	
+	// define the struc 
+	// to hold the names
 	struct Name {
-		string lastname,
+		string 
+			lastname,
 			firstname;
 
 	};
 
+	Date myDate;
 	Name myname;
 };
 
@@ -45,13 +58,13 @@ int main() {
 	cout << "Current date:" << endl << endl;
 
 	cout << "month.....";
-	cin >> today.month;
+	cin >> today.myDate.month;
 
 	cout << "day.....";
-	cin >> today.day;
+	cin >> today.myDate.day;
 
 	cout << "year.....";
-	cin >> today.year;
+	cin >> today.myDate.year;
 
 	cout << endl << endl;
 
@@ -59,20 +72,20 @@ int main() {
 	cout << "Your birthdate:" << endl << endl;
 
 	cout << "month.....";
-	cin >> theDOB.month;
+	cin >> theDOB.myDate.month;
 
 	cout << "day.....";
-	cin >> theDOB.day;
+	cin >> theDOB.myDate.day;
 
 	cout << "year.....";
-	cin >> theDOB.year;
+	cin >> theDOB.myDate.year;
 
 	mybirthdaytoday = today.isItYourBday(theDOB);
 	today.greetFuncs(mybirthdaytoday, theDOB);
 }
 
 bool ProcessDates::isItYourBday(TheDate theDOB) {
-	if (month == theDOB.month && day == theDOB.day)
+	if (myDate.month== theDOB.myDate.month && myDate.day == theDOB.myDate.day)
 		return true;
 	else
 		return false;
@@ -90,7 +103,7 @@ void ProcessDates::greetFuncs(bool mybirthdaytoday, TheDate theDOB) {
 		cout << endl << endl;
 		cout << "************************************" << endl;
 		cout << "Happy birthday!!!!!" << myname.firstname << " " << myname.lastname << endl;
-		cout << "You are now " << year - theDOB.year << " yearls old..." << endl;
+		cout << "You are now " << myDate.year- theDOB.myDate.year << " yearls old..." << endl;
 		cout << "************************************" << endl;
 	}
 	else {
@@ -99,19 +112,28 @@ void ProcessDates::greetFuncs(bool mybirthdaytoday, TheDate theDOB) {
 		cout << endl << endl;
 		cout << "Sorry. " << myname.firstname << ". " << endl;
 		cout << "Today is not your birthday....." << endl;
-		if (theDOB.month == month) {
-			if (day < theDOB.day)
-				waitdays = theDOB.day - day;
+
+		// get the number of days you have to wait/missed
+		// if the curr month is your bday
+		if (theDOB.myDate.month == myDate.month) {
+			if (myDate.day < theDOB.myDate.day)
+				waitdays = theDOB.myDate.day - myDate.day;
 			else
-				missedays = theDOB.day - theDOB.day;
+				missedays = myDate.day - theDOB.myDate.day;
 		}
-		else if (theDOB.month > month)
-			waitmonths = theDOB.month - month;
+
+		// compute the number of months you have to wait
+		// for your next bday
+		else if (theDOB.myDate.month > myDate.month)
+			waitmonths = theDOB.myDate.month - myDate.month;
 		else
-			waitmonths = 12 - (theDOB.month + month);
+			waitmonths = 12 - (theDOB.myDate.month + myDate.month);
+
+		// tell the user how long they
+		// have to wait until their bday
 		if (waitmonths > 0)
 			cout << "You need to wait " << waitmonths << " months "
-			<< " and " << theDOB.day << " before your next birthday..." << endl;
+			<< " and " << theDOB.myDate.day << " before your next birthday..." << endl;
 		if (waitdays > 0)
 			cout << "You need to wait " << waitdays << " days before your next birthday..." << endl;
 		if (missedays > 0)
